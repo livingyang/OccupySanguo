8c2e38663833f25c008d9267f130880217cf039d
livingyang <lhm.cold@163.com>
2014-02-14 19:44:33 +0800

    Add: Grunt

---
+++ grunt/dist/OccupySangou.js
@@ -0,0 +1,12 @@
+/*! OccupySangou - v0.1.0 - 2014-02-14
+* https://github.com/livingyang/OccupySanguo
+* Copyright (c) 2014 livingyang; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
---
+++ grunt/lib/OccupySangou.js
@@ -0,0 +1,17 @@
+/*
+ * OccupySangou
+ * https://github.com/livingyang/OccupySanguo
+ *
+ * Copyright (c) 2014 livingyang
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
